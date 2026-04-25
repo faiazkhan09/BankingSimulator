0dc68defb6461b3fb5753e0cc16796452c1378c6
faiaz0925 <faiaz0925@gmail.com>
2026-04-26 03:39:34 +0600

    Initializing dart project

---
+++ banking_simulator/bin/banking_simulator.dart
@@ -0,0 +1,5 @@
+import 'package:banking_simulator/banking_simulator.dart' as banking_simulator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${banking_simulator.calculate()}!');
+}
---
+++ banking_simulator/test/banking_simulator_test.dart
@@ -0,0 +1,8 @@
+import 'package:banking_simulator/banking_simulator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
