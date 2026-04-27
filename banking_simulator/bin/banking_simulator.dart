3f543ef27a37c616aea8ec47313e89bdd0276bfa
faiaz0925 <faiaz0925@gmail.com>
2026-04-27 07:02:53 +0600

    added user input

--- banking_simulator/bin/banking_simulator.dart
+++ banking_simulator/bin/banking_simulator.dart
@@ -1,3 +1,30 @@
+import 'dart:io';
 void main(){
-  print("Welcome to Faiaz bank's banking app");
+
+  bool input = false;
+
+  while(input == false){
+    print('1. Deposit \n2. Withdraw \n3. Check Balance \n0. Exit Program \nEnter the number of your choise:');
+    String? choise = stdin.readLineSync();
+     if(choise == '1'){
+    print('Checking Deposit');
+    input = true;
+    }
+    else if(choise == '2'){
+      print('checking Withdrawal');
+      input = true;
+    }
+    else if(choise == '3'){
+      print('Checking Balance');
+      input = true;
+    }
+    else if(choise == '0'){
+      print('Exiting Program');
+      input = true;
+    }
+    else{
+      print('Whatever');
+      input = false;
+    }
+  }
 }
