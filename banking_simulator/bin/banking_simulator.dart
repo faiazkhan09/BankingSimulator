28e26576ed046be985b90b1242761fbc9f5beb93
faiaz0925 <faiaz0925@gmail.com>
2026-04-28 05:24:36 +0600

    trying to implement deposit

--- banking_simulator/bin/banking_simulator.dart
+++ banking_simulator/bin/banking_simulator.dart
@@ -1,15 +1,41 @@
 import 'dart:io';
+
+int balance = 0;
 void main(){
 
   bool input = false;
+  
 
   while(input == false){
-    print('1. Deposit \n2. Withdraw \n3. Check Balance \n0. Exit Program \nEnter the number of your choise:');
+    print('1. Deposit \n2. Withdraw \n3. Check Balance \n0. Exit Program \n');
+    stdout.write('Enter the number of your choise:');
     String? choise = stdin.readLineSync();
-     if(choise == '1'){
-    print('Checking Deposit');
-    input = true;
+    
+    if(choise == '1'){
+      stdout.write('Enter the amount to deposit:');
+      String? amountD = stdin.readLineSync();
+
+      if(amountD != null){
+        int amountDT = int.parse(amountD);
+        if(amountDT > 0){
+          balance += amountDT;
+          print('Amount Deposited');
+          stdout.write('Continue Trancaction?(Y/n)');// enter to continue, type n to go back to home and anything else gives the massage invalid input and asks again
+          String? conChoise = stdin.readLineSync();
+          if(conChoise != null && conChoise == 'n'){
+            input = true;
+          }
+          else{
+            input = true;
+          }
+        }
+      }
+      else{
+        print('No number provided. Going back to home page.');
+        input = false;
+      }
     }
+    
     else if(choise == '2'){
       print('checking Withdrawal');
       input = true;
