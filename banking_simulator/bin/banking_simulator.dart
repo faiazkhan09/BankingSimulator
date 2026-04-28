c8dd2d8bdceeb2da8411cb9cf5cf3dfff59fa0ff
FAIAZ09\DELL <faiaz0925@gmail.com>
2026-04-29 00:04:25 +0600

    deposit now fully works :)

--- banking_simulator/bin/banking_simulator.dart
+++ banking_simulator/bin/banking_simulator.dart
@@ -1,54 +1,71 @@
 import 'dart:io';
 
-int balance = 0;
-void main(){
-
+double balance = 0;
+void main() {
   bool input = false;
-  
 
-  while(input == false){
-    print('1. Deposit \n2. Withdraw \n3. Check Balance \n0. Exit Program \n');
+  while (input == false) {
+    print('\n1. Deposit \n2. Withdraw \n3. Check Balance \n0. Exit Program \n');
     stdout.write('Enter the number of your choise:');
     String? choise = stdin.readLineSync();
-    
-    if(choise == '1'){
-      stdout.write('Enter the amount to deposit:');
-      String? amountD = stdin.readLineSync();
 
-      if(amountD != null){
-        int amountDT = int.parse(amountD);
-        if(amountDT > 0){
-          balance += amountDT;
-          print('Amount Deposited');
-          stdout.write('Continue Trancaction?(Y/n)');// enter to continue, type n to go back to home and anything else gives the massage invalid input and asks again
-          String? conChoise = stdin.readLineSync();
-          if(conChoise != null && conChoise == 'n'){
-            input = true;
+    if (choise == '1') {
+      bool depositConti = true;
+      while (depositConti == true) {
+        stdout.write('\nEnter the amount to deposit:');
+        String? amountD = stdin.readLineSync();
+
+        if (amountD != null) {
+          double? amountDT = double.tryParse(
+            amountD,
+          ); //instead of using .Parse which would try to parse actual string into double, we can use tryParse,which would return null if it can't successfully parse.
+          if (amountDT == null) {
+            print('Invalid input! Please enter a numner');
+            depositConti = true;
+          } else if (amountDT > 0) {
+            balance += amountDT;
+            print('\nAmount Deposited');
+
+            bool depositTransacConti = true;
+            while (depositTransacConti == true) {
+              stdout.write(
+                '\nContinue Trancaction?(Y/n)',
+              ); // enter to continue, type n to go back to home and anything else gives the massage invalid input and asks again
+              String? conChoise = stdin.readLineSync();
+
+              if (conChoise != null && conChoise == 'n') {
+                depositTransacConti = false;
+                depositConti = false;
+              } else if (conChoise == '' ||
+                  conChoise == 'Y' ||
+                  conChoise == 'y') {
+                depositConti = true;
+                depositTransacConti = false;
+              } else {
+                print('Invalid input, please provide correct input!');
+                depositTransacConti = true;
+              }
+            }
+          } else if (amountDT < 0) {
+            print('Enter amount greater than 0');
+            depositConti = true;
           }
-          else{
-            input = true;
-          }
+        } else {
+          print('No iput provided!');
+          depositConti = true;
         }
       }
-      else{
-        print('No number provided. Going back to home page.');
-        input = false;
-      }
-    }
-    
-    else if(choise == '2'){
+    } else if (choise == '2') {
       print('checking Withdrawal');
       input = true;
-    }
-    else if(choise == '3'){
-      print('Checking Balance');
-      input = true;
-    }
-    else if(choise == '0'){
+    } else if (choise == '3') {
+      print('\nChecking Balance...');
+      print('Balance available: $balance');
+      input = false;
+    } else if (choise == '0') {
       print('Exiting Program');
       input = true;
-    }
-    else{
+    } else {
       print('Whatever');
       input = false;
     }
